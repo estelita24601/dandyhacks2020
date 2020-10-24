1074cda8d431cae3ac58c64dfea4cf6eed659d6d
estelita24601 <estelitac24401@hotmail.com>
2020-10-24 12:50:44 -0400

    card data from google docs

---
+++ CardData.java
@@ -0,0 +1,14 @@
+public class CardData{
+    private int IDNumber;
+    private DateTime lastCheck;
+
+    public int getID(){
+        return IDNumber;
+    }
+
+    public void setID(int inputID){
+        this.IDNumber = inputID;
+    }
+
+    public
+}
